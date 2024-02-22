7c6b7491cbe54334b6272f25cd758db581d2c42f
Sanket Kurkute <145901190+sanketkurkute888@users.noreply.github.com>
2024-02-22 13:05:21 +0530

    Add files via upload

---
+++ 12-07-23_C_BasicProgram5/tempCodeRunnerFile.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+void main()
+{
+    char str[50],str1[50], choice;
+    int i, ctr = 0,flag=0;
+
+    printf("Enter the string: ");
+    gets(str);
+
+
+    printf("Enter word you want to find: ");
+    // scanf("%c", &choice);
+      gets(str1);
+
+    for (i = 0; str[i] != '\0'; i++)
+    {
+        if (str1[i] == str[i])
+        {
+            // ctr++;
+
+            flag=1;
+        }
+    }
+    str[i]='\0';
+    str1[i]='\0';
+    // printf("%d",flag);
+    if(flag==1)
+    {
+    // printf("Yes present :%d times", ctr);
+    printf("Enter word found.");
+    }
+    else
+    {
+        printf("Not found");
+    }
+}
