05522855b3ef21a3c95920f0301671db70e64992
Sanket Kurkute <145901190+sanketkurkute888@users.noreply.github.com>
2024-02-22 12:32:07 +0530

    Add files via upload

---
+++ 06-06-23_ForLoopExample1/for_loop_demo.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main()
+{
+
+    //    for (int i = 1; i <= 5; i++)
+    //    {
+    //    /* code here */
+    //    printf("Hello %d\n", i);
+    //    }
+
+    /*Nested for loop*/
+    // for (int i = 0; i <= 3; i++)
+    // {
+    //     /* code here */
+    //     printf("\n%d outer loop\n",i);
+    //     for (int j = 0; j <=2 ; j++)
+    //     {
+    //         /* code here */
+    //         printf("%d\n",j);
+
+
+    //     }
+    // }
+
+    
+}
